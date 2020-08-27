1ec0d01a9a21a222ec2d23aae0f0cab577a15e41
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-08-27 18:44:00 -0500

    master circle area rounding to ceiling

--- exercise/circle_area/circle_area.py
+++ exercise/circle_area/circle_area.py
@@ -1,7 +1,7 @@
 import math
 
 def calculate_circle_area(radius):
-    return math.pi * radius**2
+    return math.ceil(math.pi * radius**2)
 
 radius = float( input() )
 area = calculate_circle_area(radius)
